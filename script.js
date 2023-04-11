a9c53a0e65a6c473bf030613e4ddeb26830664a1
Samta <108336676+Smta028@users.noreply.github.com>
2023-04-11 16:33:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	return{
+		name:name;
+		age:age;
+	}
+greet(){
+	console.log("Hello,my name is"+Person.name+", I am"+Person.age+"years old.")
+}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	super()
+	return{
+		
+		jobTitle:jobTitle;
+	}
+	jobGreet(){
+		console.log("Hello,my name is"+Employee.name+", I am"+Employee.age+"years old,and my job title is"+Employee.jobTitle);
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
